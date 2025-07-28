3dc3389f7641fe17ae83c79b5591afb3d8f025b1
Humberto Massa <humbertomassa@gmail.com>
2025-07-28 12:56:33 -0300

    mcp,csv

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -3,6 +3,8 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+  build = "bundled_build.lua",
+  use_bundled_binary = true,
   config = function() require("mcphub").setup() end,
 }
+-- 2. For bundled install: Set build = 'bundled_build.lua' in lazy spec and use_bundled_binary = true in config.
